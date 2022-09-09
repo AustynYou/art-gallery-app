3a358a22f08f7395e0449af6440a1915eae0f3fd
AustynYou <austyn.you@gmail.com>
2022-09-09 16:36:54 -0700

    💣 update files location, remove some files

--- back/src/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-
-const port = 8000;
-
-app.listen(port, (req, res) => {
-  console.log(port + "번에서 서버 실행 중");
-});
--- front/src/apis/user.js
+++
@@ -1,9 +0,0 @@
-import instance from ".";
-
-export const createUser = (data) => {
-  instance({
-    method: "POST",
-    url: "/users",
-    data,
-  });
-};
