847674debed8a97bdae90812464d50a4bebb05a3
AustynYou <austyn.you@gmail.com>
2022-09-09 16:20:08 -0700

    🔧 import express, routers

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+import postsRouter from "./routes/posts.js";
+import uploadRouter from "./routes/upload.js";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cors(["http://localhost:3000"]));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+app.use("/upload", uploadRouter);
+
+export default app;
