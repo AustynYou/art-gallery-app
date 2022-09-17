f119a818d7555932b8bf25bfc884a32ca075a0cd
AustynYou <austyn.you@gmail.com>
2022-09-16 20:45:52 -0700

    🚀 update for production

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import hpp from "hpp";
+import helmet from "helmet";
 
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
@@ -9,9 +11,15 @@ import uploadRouter from "./routes/upload.js";
 
 const app = express();
 
-app.use(logger("dev"));
+if (process.env.NODE_ENV === "production") {
+  app.use(logger("combined"));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(logger("dev"));
+}
 app.use(express.json());
-app.use(cors(["http://localhost:3000"]));
+app.use(cors(["http://localhost:3000", "art-gallery-app"]));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
